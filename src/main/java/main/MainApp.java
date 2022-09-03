39a0dec27bfb347039e375b26291a9058ffd4311
Dana Hortopan <dana.hortopan@yahoo.com>
2022-09-03 13:38:23 +0300

    test

--- src/main/java/main/MainApp.java
+++ src/main/java/main/MainApp.java
@@ -1,9 +1,13 @@
 package main;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MainApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(MainApp.class, args);
     }
 
 
